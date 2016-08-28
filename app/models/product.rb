ae1e597652e1fbfd12febd75ff907373d58d4d75
STEPHANIE SUTANTO <stph.sutanto@gmail.com>
2016-08-28 21:15:25 +0800

    changed view

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
 	has_one :bid
 	belongs_to :user
+	validates :title, :presence => true
+	validates :description, :presence => true
+	validates :starting_bid, :presence => true
+	has_many :bids
 end
